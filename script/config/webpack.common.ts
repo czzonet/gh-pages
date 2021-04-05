63eda2960fb577df164f775294a78de2985f55df
czzonet <czzonet@163.com>
2021-04-05 22:01:41 +0800

    fix

--- script/config/webpack.common.ts
+++ script/config/webpack.common.ts
@@ -8,7 +8,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import CopyWebpackPlugin from "copy-webpack-plugin";
 
 const publicPath = process.env.NODE_ENV ? "/gh-pages" : "/";
-const publicPathSvg = process.env.NODE_ENV ? "gh-pages" : undefined;
+// const publicPathSvg = process.env.NODE_ENV ? "gh-pages" : undefined;
 
 export const commonConfig: Configuration = {
   context: projectRoot,
